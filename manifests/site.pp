8a1ff33b03d6415c7cdb7200e567623db511a985
omkarphadke84 <omkarphadke@gmail.com>
2020-04-09 00:38:49 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,5 @@
 node default {
-  file {'/root/README':
-    ensure => file,
-    content => 'this is a readme',
-    owner => 'nobody'
-  }
+}
+node 'master.puppet.vm' {
+  include role::master_server
 }
